094c51e923b8b588f644bca8906853d72a2529b5
soabel <alfredo.benaute@gmail.com>
2020-12-01 15:14:02 -0500

    config microservice

---
+++ src/test/java/com/bootcamp/adventureworks/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.adventureworks.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
